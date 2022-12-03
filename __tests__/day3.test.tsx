57dcd971ce7040877c09a888d0d66fce0cfe8a27
lperry25 <10005215+lperry25@users.noreply.github.com>
2022-12-03 16:33:54 +0100

    fix bug in part 2

--- __tests__/day3.test.tsx
+++ __tests__/day3.test.tsx
@@ -25,4 +25,28 @@ describe('Day 3', () => {
     expect(result.result1).toBe(157);
     expect(result.result2).toBe(70);
   });
+
+  it('passes the test case 2', async () => {
+    const response = await solve(
+      'RCMRQjLLWGTjnlnZwwnZJRZH\n' +
+        'qnvfhpSbvSppNddNdSqbbmmdPrwttJVrVPDVrJtHtwPZhrPJ\n' +
+        'BFpFzSSqSFFSvQsnWgCMjTLzng\n' +
+        'DbWVcVRRdlLffvtqjTWNgQ\n' +
+        'DbWVcVRRdlLffvtqjTWNgQ\n' +
+        'mJJMpsmrMrJSHJpsHrFHvBvgHvqfNvzffgTvfj\n' +
+        'mMhPjmjmFPJhMSGGcDRlwRdcLGPc\n' +
+        'qFcbmWFJqqWpRJcQWpqsQQQwSPCPrHRHCPdNZtSrSHwrNZ\n' +
+        'jGMjGLhhhgTvghgtGVjnNCrPVwZSZffSNSwHZZdH\n' +
+        'DvzDlvvhnjlMlglglGGhDLpqqcJWWtsmszpWbBBBmQmb\n' +
+        'SPLPHQbJSbPsvTLmfDvVDctvWhcDlD\n' +
+        'jdRRzzGgJqwrpMRMgdjlcVcWqfWWlfDlmmlWhB\n' +
+        'rwgRGdpGprNNLQLsbZJPsn\n' +
+        'GZhTVLztHrSzrRBz\n' +
+        'MJWjMvsfSCLSnrJn\n' +
+        'MjglcgWMdccvZGFtTDchLLLh',
+    );
+    // @ts-ignore
+    const result = JSON.parse(response._getData());
+    expect(result.result2).toBe(110);
+  });
 });
